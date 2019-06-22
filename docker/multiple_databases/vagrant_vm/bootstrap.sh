15c3237faa08a06de3dab6f241e2c759eefb6715
Mammad Shahmaliyev <mamed.shahmaliyev@gmail.com>
2019-06-22 23:41:23 +0400

    vagrant vm folder created

--- docker/multiple_databases/vagrant_vm/bootstrap.sh
+++ docker/multiple_databases/vagrant_vm/bootstrap.sh
@@ -30,4 +30,5 @@ yum install -y curl
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-chmod 777 /docker/oracle/data # to avoid oracle permission issues
+## install additional utilities
+yum install nano tree -y
