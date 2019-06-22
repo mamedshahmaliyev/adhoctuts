49a14d17487add3dac6f286febf7de4703fd7e17
Mammad Shahmaliyev <mamed.shahmaliyev@gmail.com>
2019-06-22 17:21:03 +0400

    Vagrant file added

---
+++ docker/multiple_databases/bootstrap.sh
@@ -0,0 +1,33 @@
+yum update && yum upgrade
+
+## install docker, https://docs.docker.com/install/linux/docker-ce/centos/
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+				  
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+  
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+	
+sudo yum-config-manager --enable docker-ce-nightly
+
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+## install docker-compose, https://docs.docker.com/compose/install/
+yum install -y curl
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+chmod 777 /docker/oracle/data # to avoid oracle permission issues
---
+++ docker/multiple_databases/useful_commands.sh
@@ -0,0 +1,37 @@
+#####################################################################
+## docker-compose commands, works with service names ################
+## docker-compose will look for docker-compose.yml file by default ##
+#####################################################################
+
+docker-compose up -d # build and start all the containers
+docker-compose up -d service_name # build and start specific container
+
+# stop and remove all the containers, 
+# removes all the data unless the data persistence is specified
+docker-compose down 
+docker-compose rm -f -s service_name # remove the specific container
+
+docker-compose down && docker-compose up -d # recreate all the containers
+
+docker-compose start # start all the containers
+docker-compose start service_name # start specific container
+docker-compose stop # stop all the containers
+docker-compose stop service_name # stop specific container
+
+docker-compose up --no-start # create but do not start all the containers
+
+docker-compose logs service_name # view container logs
+
+#################################################
+## docker commands, works with container names ##
+#################################################
+
+docker ps -a # view the container list
+docker exec -it container_name bash # access command line of the container
+docker start $(docker ps -aq) # start all the containers
+docker start container_name # stop the specific container
+docker stop container_name # stop the specific container
+docker rm -f container_name # stop and remove containers
+
+# execute specific script from within container
+docker exec -it container_name sh -c 'exec /scripts/my.sh'
